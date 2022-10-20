0077c18acaffba66cb6ef742f9da766bc4adfdaf
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-10-19 22:43:26 -0500

    terminando practica de patrones - api rest

--- Practica-ApiRest/index.js
+++ Practica-ApiRest/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const services = require('./services');
-const handlers = require('./handlers');
+const handlify = require('./handlers');
 
 const port = 3000;
 const app = express();
 
+app.use(express.json());
 
-app.get('/', handlers(services).get);
+const usersHandler = handlify('/users');
+
+
+app.get('/', usersHandler(services).get);
+app.post('/', usersHandler(services).post);
+app.put('/:id', usersHandler(services).put);
+app.delete('/:id', usersHandler(services).delete);
 
 
 app.listen(port, () => {
